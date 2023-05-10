5dd9e926434fce90d3507c4b21ef8ec44af3b829
Arjun Menon <arjun.mnn17@gmail.com>
2023-05-10 18:26:25 -0400

    navbar v1

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-left">
+          <Link to="/" className="navbar-logo">
+            Wealth Sim
+            <img
+              src="/src/assets/logo.png"
+              alt="WealthSim"
+              className="navbar-logo"
+            />
+          </Link>
+        </div>
+        <ul className="navbar-right">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about" className="nav-link">
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/learn" className="nav-link">
+              Learn
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/login" className="nav-link">
+              Log In
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/signup" className="nav-link">
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,8 @@
+import "../../App.css";
+import React from "react";
+
+function Home() {
+  return <></>;
+}
+
+export default Home;
